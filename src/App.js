ea1561a8e15eaada3a1d419618df48c6eb8b9844
tedw87 <teodorvoicu87@gmail.com>
2023-04-05 15:53:36 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
